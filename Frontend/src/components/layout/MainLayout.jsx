8993a1b93f51fbeb097bd448cfb7d2aa40a0ab5a
Mehboob Ali <mehboobaliali150@gmail.com>
2025-07-21 06:18:48 +0500

    navbar issue resolve

--- Frontend/src/components/layout/MainLayout.jsx
+++ Frontend/src/components/layout/MainLayout.jsx
@@ -1,15 +1,27 @@
+
 import Navbar from './Navbar'
 import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
+import { useState } from 'react'
+import Login from '../../auth/Login'
+
 
 const MainLayout = () => {
+  const [authModal, setAuthModal] = useState(null) // 'login' or null
+
+  const handleOpenAuth = (type) => setAuthModal(type)
+  const handleCloseAuth = () => setAuthModal(null)
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
-      <Navbar />
+      <Navbar onOpenAuth={handleOpenAuth} />
       <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
+      {authModal === 'login' && (
+        <Login isOpen={true} onClose={handleCloseAuth} />
+      )}
     </div>
   )
 }
